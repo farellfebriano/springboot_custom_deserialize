f568c023ff3bce4d2e0dab2a6fb290ecdaf673a7
farellfebriano <farellfebriano8@gmail.com>
2024-06-14 09:14:12 -0500

    init spring

---
+++ custom-deserializer/src/main/java/dev/farell/custom_deserializer/CustomDeserializerApplication.java
@@ -0,0 +1,13 @@
+package dev.farell.custom_deserializer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomDeserializerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomDeserializerApplication.class, args);
+	}
+
+}
---
+++ custom-deserializer/src/test/java/dev/farell/custom_deserializer/CustomDeserializerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.farell.custom_deserializer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomDeserializerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
